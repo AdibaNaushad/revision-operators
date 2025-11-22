193d2e006f6e421475148e622ca155b56edd619f
AdibaNaushad <adibanaushad0@gmail.com>
2025-11-22 16:31:56 +0530

    Create relational.java

---
+++ relational.java
@@ -0,0 +1,14 @@
+public class relationalOperater {
+    public static void main(String[] args) {
+        int p = 2, q = 4;
+
+        System.out.println(p == q);  //f
+        System.out.println(p != q); //t
+        System.out.println(p <= q); //t
+        System.out.println(p >= q); //f
+        System.out.println(p < q); //t
+        System.out.println(p > q); //f
+
+    }
+
+}
