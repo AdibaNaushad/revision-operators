d7ca33609a08573c0333a592618a66a3e7f5b25d
AdibaNaushad <adibanaushad0@gmail.com>
2025-11-22 16:40:32 +0530

    Create assignment.java

---
+++ assignment.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class assignment {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int p = sc.nextInt();
+        int q = sc.nextInt();
+
+        System.out.println(p += q);
+        System.out.println(p -= q);
+        System.out.println(p *= q);
+        System.out.println(p /= q);
+
+    }
+
+}
